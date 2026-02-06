d5bd22bde52b38f8f1741a718758a86d274d777d
Daniel Velasquez Neira <42185968+dannyvelas@users.noreply.github.com>
2026-02-05 21:40:08 -0500

    fixed config bug M config/plex.yml M internal/handlers/terraform_plex_config.go

--- internal/handlers/terraform_plex_config.go
+++ internal/handlers/terraform_plex_config.go
@@ -11,8 +11,8 @@ type terraformPlexConfig struct {
 	GatewayAddress             string `json:"gateway_address" required:"true"`
 	IP                         string `json:"ip" required:"true"`
 	Node                       string `json:"node" required:"true"`
-	SSHUser                    string `json:"ssh_user" required:"true"`
-	SSHRealm                   string `json:"ssh_realm" required:"true"`
+	SSHUser                    string `json:"-" required:"true" conflux:"ssh_user"`
+	SSHRealm                   string `json:"-" required:"true" conflux:"ssh_realm"`
 	Password                   string `json:"password" required:"true" conflux:"proxmox_root_password"`
 	SSHPublicKeyPath           string `json:"ssh_public_key_path" required:"true"`
 	TerraformVersionConstraint string `json:"-" required:"true" conflux:"terraform_version_constraint"`
