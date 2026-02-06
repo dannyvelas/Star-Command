6e65aa2a1ff287276d12cc1ed3352cd152eb02be
Daniel Velasquez Neira <42185968+dannyvelas@users.noreply.github.com>
2026-02-06 12:38:08 -0500

    merged M terraform/incus/global/profiles.tf D terraform/incus/global/provider.tf

--- terraform/incus/global/profiles.tf
+++ terraform/incus/global/profiles.tf
@@ -1,9 +1,25 @@
+terraform {
+  required_providers {
+    incus = {
+      source  = "lxc/incus"
+      version = ">= 1.0.0"
+    }
+  }
+  required_version = "~> 1.13.3"
+}
+
+provider "incus" {
+  remote {
+    name = var.incus_remote
+  }
+}
+
 locals {
   ssh_key = trimspace(file(var.ssh_public_key_path))
 }
 
 resource "incus_profile" "basic" {
-  name = "basic"
+  name        = "basic"
   description = "Basic networking and root disk configuration"
 
   device {
@@ -26,7 +42,7 @@ resource "incus_profile" "basic" {
 }
 
 resource "incus_profile" "management" {
-  name = "management"
+  name        = "management"
   description = "Management access (SSH keys)"
 
   config = {
